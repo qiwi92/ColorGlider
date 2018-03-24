58bca200c1ae6aa0f5b923409e9ea0323439ba25
Josh <xell.art@hotmail.com>
2018-03-24 19:41:21 +0100

    Added scene switching with shop

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+
+    public Button Button;
+
+    void Start()
+    {
+        Button.onClick.AddListener(ClickAnimation);
+    }
+
+
+    public void ClickAnimation()
+    {
+        transform.DOPunchScale(Vector3.one * 0.3f, 0.1f).SetEase(Ease.InOutBounce).OnComplete(() =>
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        });
+
+
+    }
+}
+
+
---
+++ Assets/ShopButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ShopButton : MonoBehaviour
+{
+    public Button Button;
+
+    private void Start()
+    {
+        Button.onClick.AddListener(ClickAnimation);
+    }
+    public void ClickAnimation()
+    {
+        transform.DOPunchScale(Vector3.one * 0.3f, 0.1f).SetEase(Ease.InOutBounce).OnComplete(() =>
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        });
+
+
+    }
+}
