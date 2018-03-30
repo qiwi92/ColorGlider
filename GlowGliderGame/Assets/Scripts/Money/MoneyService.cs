bec6f95ccc6ab7f46d9600acbf876c99c0d1768e
Jeremy <jeremy.ries@fluffyfairygames.com>
2018-03-30 16:29:54 +0200

    added MoneyService

---
+++ GlowGliderGame/Assets/Scripts/Money/MoneyService.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Money
+{
+    public class MoneyService
+    {
+        private static MoneyService _instance;
+        public static MoneyService Instance
+        {
+            get { return _instance ?? (_instance = new MoneyService()); }
+        }
+
+        private int _money;
+
+        public int CurrentMoney
+        {
+            get { return _money; }
+        }
+
+        public void AddMoney(int amount)
+        {
+            _money += amount;
+        }
+
+        public void SubtractMoney(int amount)
+        {
+            if (_money - amount < 0)
+            {
+                Debug.LogWarning("Subtracted more money than available");
+                _money = 0;
+            }
+
+            _money -= amount;
+        }
+    }
+}
---
+++ GlowGliderGame/Assets/StompyRobot/SRDebugger/Scripts/SROptions.Money.cs
@@ -0,0 +1,14 @@
+﻿using Money;
+
+public partial class SROptions
+{
+    public void RichBitch()
+    {
+        MoneyService.Instance.AddMoney(10000000);
+    }
+
+    public string CurrentCash
+    {
+        get { return MoneyService.Instance.CurrentMoney.ToString(); }
+    }
+}
