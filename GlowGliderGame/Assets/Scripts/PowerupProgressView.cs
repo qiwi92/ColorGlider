2a09948f596be8b0c0b6dd2964598cfaf339f0fe
Josh <xell.art@hotmail.com>
2018-03-31 00:42:17 +0200

    Added Color setter to shop, layering and raycasting

--- GlowGliderGame/Assets/Scripts/PowerupProgressView.cs
+++ GlowGliderGame/Assets/Scripts/PowerupProgressView.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
     public class PowerupProgressView : MonoBehaviour
     {
-        [SerializeField]
-        private Image[] _progressImages;
+        [SerializeField] private Image[] _progressImages;
+        private Color _color;
 
-        public void Init()
+        public void Init(Color color)
         {
+            _color = color;
             foreach (var progressImage in _progressImages)
             {
                 progressImage.color = Color.black;               
@@ -20,7 +22,7 @@ namespace Assets.Scripts
         {
             for (int i = 0; i < current; i++)
             {
-                _progressImages[i].color = Color.green;
+                _progressImages[i].color = _color;
             }
         }
     }
