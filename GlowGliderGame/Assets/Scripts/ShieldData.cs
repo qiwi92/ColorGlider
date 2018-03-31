ce73c30251dd21cddb42ab307186fd8931ce8f44
Josh <xell.art@hotmail.com>
2018-03-31 13:09:15 +0200

    Added boost effect visuals

---
+++ GlowGliderGame/Assets/Scripts/ShieldData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ShieldData : IPowerupData
+    {
+        public float GetSpawnChance(int level)
+        {
+            return Mathf.Clamp(level * 0.1f , 0 , 0.5f);
+        }
+
+        public float GetCost(int level)
+        {
+            return 20*Mathf.Pow(1.1f, level);
+        }
+    }
+}
